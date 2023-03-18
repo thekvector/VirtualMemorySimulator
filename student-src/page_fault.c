e8b6c3f8151c9a4c316bbd96d322f1c5e42086b6
thekvector <74088461+thekvector@users.noreply.github.com>
2023-03-17 20:00:51 -0400

    Update page_fault.c

--- student-src/page_fault.c
+++ student-src/page_fault.c
@@ -6,30 +6,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-/*  --------------------------------- PROBLEM 6 --------------------------------------
-    Checkout PDF section 7 for this problem
 
-    Page fault handler.
-
-    When the CPU encounters an invalid address mapping in a page table,
-    it invokes the OS via this handler.
-
-    Your job is to put a mapping in place so that the translation can
-    succeed. You can use free_frame() to make an available frame.
-    Update the page table with the new frame, and don't forget
-    to fill in the frame table.
-
-    Lastly, you must fill your newly-mapped page with data. If the page
-    has never mapped before, just zero the memory out. Otherwise, the
-    data will have been swapped to the disk when the page was
-    evicted. Call swap_read() to pull the data back in.
-
-    HINTS:
-         - You will need to use the global variable current_process when
-           setting the frame table entry.
-
-    ----------------------------------------------------------------------------------
- */
 void page_fault(vaddr_t address) {
     /* First, split the faulting address and locate the page table entry.
        Remember to keep a pointer to the entry so you can modify it later. */
